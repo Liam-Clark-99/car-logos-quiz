4e45209ca84a58caf1e938d03f2600e15d897e36
Liam-Clark-99 <>
2023-10-07 16:59:52 +0000

    added array for car logos and their repective names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log('hellow world!');
+const logos = [
+    {src: "/assets/images/audi-logo.png", name: "Audi"},
+    {src: "/assets/images/bentley.logo.png", name: "Bentley"},
+    {src: "/assets/images/chevrolet.logo.png", name: "Chevrolet"},
+    {src: "/assets/images/citroen-logo.png", name: "Citroen"},
+    {src: "/assets/images/dodge-logo.png", name: "Dodge"},
+    {src: "assets/images/ferrari-logo.png", name: "Ferrari"},
+    {src: "assets/images/lexus-logo.png", name: "Lexus"},
+    {src: "assets/images/mercedes-logo.png", name: "Mercedes"},
+    {src: "assets/images/peugeot-logo.png", name: "Peugeot"},
+    {src: "assets/images/toyota-logo.png", name: "Toyota"}
+]
