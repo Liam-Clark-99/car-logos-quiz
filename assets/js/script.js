d11c77c79ec2ae63dd2f9cc2c161d5edb6f75cf1
Liam-Clark-99 <>
2023-10-08 11:12:34 +0000

    edited file paths for images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 const logos = [
-    { src: "/assets/images/audi-logo.jpg", name: "Audi" },
-    { src: "/assets/images/bentley-logo.jpg", name: "Bentley" },
-    { src: "/assets/images/chevrolet-logo.jpg", name: "Chevrolet" },
-    { src: "/assets/images/citroen-logo.jpg", name: "Citroen" },
-    { src: "/assets/images/dodge-logo.jpg", name: "Dodge" },
-    { src: "/assets/images/ferrari-logo.jpg", name: "Ferrari" },
-    { src: "/assets/images/lexus-logo.jpg", name: "Lexus" },
-    { src: "/assets/images/mercedes-logo.jpg", name: "Mercedes" },
-    { src: "/assets/images/peugeot-logo.jpg", name: "Peugeot" },
-    { src: "/assets/images/toyota-logo.jpg", name: "Toyota" }
+    { src: "assets/images/audi-logo.jpg", name: "Audi" },
+    { src: "assets/images/bentley-logo.jpg", name: "Bentley" },
+    { src: "assets/images/chevrolet-logo.jpg", name: "Chevrolet" },
+    { src: "assets/images/citroen-logo.jpg", name: "Citroen" },
+    { src: "assets/images/dodge-logo.jpg", name: "Dodge" },
+    { src: "assets/images/ferrari-logo.jpg", name: "Ferrari" },
+    { src: "assets/images/lexus-logo.jpg", name: "Lexus" },
+    { src: "assets/images/mercedes-logo.jpg", name: "Mercedes" },
+    { src: "assets/images/peugeot-logo.jpg", name: "Peugeot" },
+    { src: "assets/images/toyota-logo.jpg", name: "Toyota" }
 ];
 
 let questionNumber = 1;
