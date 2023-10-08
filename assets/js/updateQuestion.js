e990ff8fbcc6ce57ad844a1e5cf4082cd7863e62
Liam-Clark-99 <>
2023-10-08 11:08:49 +0000

    edited script file path

--- assets/js/updateQuestion.js
+++
@@ -1,6 +0,0 @@
-function updateQuestion() {
-    const randomLogo = getRandomLogo();
-    console.log(randomLogo.name);
-    console.log(randomLogo.src);
-    document.getElementById("logo-image").src = randomLogo.src;
-}
